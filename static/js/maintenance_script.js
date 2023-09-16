264c94f6ae1ef3c933b4ced7ae38d592ea2a0cfd
yoshiayu <yoshiayu19771106@gmail.com>
2023-09-16 11:31:31 +0900

    14thcommit

--- static/js/maintenance_script.js
+++ static/js/maintenance_script.js
@@ -1,71 +1,53 @@
-console.log(document.getElementById('save-maintenance'));
-console.log(document.getElementById('export-maintenance-excel'));
-
 document.addEventListener("DOMContentLoaded", function() {
-    const saveBtn = document.getElementById('save-maintenance');
     const exportBtn = document.getElementById('export-maintenance-excel');
-    const dateInput = document.querySelector('input[type="date"]');
-
-    // 日付のフォーマットを修正
-    if (dateInput && dateInput.value) {
-        const matches = dateInput.value.match(/(\d{4})年(\d{1,2})月(\d{1,2})日/);
-        if (matches) {
-            dateInput.value = `${matches[1]}-${String(matches[2]).padStart(2, '0')}-${String(matches[3]).padStart(2, '0')}`;
-        }
-    }
-
-    saveBtn.addEventListener('click', function() {
-        // 保存処理を実装する
-    });
-
-    exportBtn.addEventListener('click', function() {
-        console.log("Export button clicked!");
-        exportToExcel();
-    });
+    exportBtn.addEventListener('click', exportToExcel);
 });
 
-// 既存のxlsxファイルを読み込む関数
 function readExistingXLSXFile(filePath) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
         xhr.open('GET', filePath, true);
         xhr.responseType = 'arraybuffer';
-
         xhr.onload = function () {
             if (xhr.status === 200) {
                 const data = new Uint8Array(xhr.response);
-                const workbook = XLSX.read(data, {type: 'array'});
+                const workbook = XLSX.read(data, {type: 'array', cellStyles: true, cellDates: true});
                 resolve(workbook);
             } else {
                 reject(`Failed to load the file. Status: ${xhr.status}`);
             }
         };
-
         xhr.onerror = function() {
             reject('An error occurred while reading the file.');
         };
-
         xhr.send();
     });
 }
 
 function exportToExcel() {
-    // 既存のExcelファイルを読み込む
-    readExistingXLSXFile('/static/飛行日誌1.xlsx').then(workbook => {
-        // 既存のWorkbookをベースとして、新しいSheetを追加する
-        const ws = XLSX.utils.table_to_sheet(document.querySelector('.maintenance-table'));
-        XLSX.utils.book_append_sheet(workbook, ws, "MaintenanceRecord");
+    readExistingXLSXFile('/static/飛行日誌1.xlsx').then(originalWorkbook => {
+        const originalSheet = originalWorkbook.Sheets[originalWorkbook.SheetNames[0]];
 
-        // ○印を入れる
-        for (let row = 1; row <= 9; row++) {
-            const checkbox = document.querySelector(`.maintenance-table tbody tr:nth-child(${row}) td:nth-child(3) input`);
-            if (checkbox && checkbox.checked) {
-                ws[XLSX.utils.encode_cell({c: 2, r: row})].v = "○";
+        // Select checkboxes from the HTML table
+        const checkboxes = document.querySelectorAll('.maintenance-table tbody tr td:nth-child(3) input');
+        const remarks = document.querySelectorAll('.maintenance-table tbody tr td:nth-child(4) input');
+
+        // Ensure only the first 9 checkboxes are considered, corresponding to the specified fields
+        for (let i = 0; i < 9; i++) {
+            if (checkboxes[i] && checkboxes[i].checked) {
+                const cellRef = XLSX.utils.encode_cell({c: 2, r: 5 + i}); // "結果" starts from C6 in Excel
+                originalSheet[cellRef] = {v: "○", t: 's', s: (originalSheet[cellRef] || {}).s};
+            }
+            if (remarks[i]) {
+                const remarkText = remarks[i].value.trim();
+                if (remarkText) {
+                    const cellRefRemark = XLSX.utils.encode_cell({c: 3, r: 5 + i}); // "備考" starts from D6 in Excel
+                    originalSheet[cellRefRemark] = {v: remarkText, t: 's', s: (originalSheet[cellRefRemark] || {}).s};
+                }
             }
         }
 
-        // 新しいExcelファイルとして保存
-        XLSX.writeFile(workbook, "Modified飛行日誌1.xlsx");
+        XLSX.writeFile(originalWorkbook, "Modified飛行日誌1.xlsx");
     }).catch(error => {
         console.error(error);
     });
--- staticfiles/js/maintenance_script.js
+++ staticfiles/js/maintenance_script.js
@@ -1,72 +1,108 @@
-console.log(document.getElementById('save-maintenance'));
-console.log(document.getElementById('export-maintenance-excel'));
-
 document.addEventListener("DOMContentLoaded", function() {
-    const saveBtn = document.getElementById('save-maintenance');
     const exportBtn = document.getElementById('export-maintenance-excel');
-    const dateInput = document.querySelector('input[type="date"]');
-
-    // 日付のフォーマットを修正
-    if (dateInput && dateInput.value) {
-        const matches = dateInput.value.match(/(\d{4})年(\d{1,2})月(\d{1,2})日/);
-        if (matches) {
-            dateInput.value = `${matches[1]}-${String(matches[2]).padStart(2, '0')}-${String(matches[3]).padStart(2, '0')}`;
-        }
-    }
-
-    saveBtn.addEventListener('click', function() {
-        // 保存処理を実装する
-    });
-
-    exportBtn.addEventListener('click', function() {
-        console.log("Export button clicked!");
-        exportToExcel();
-    });
+    exportBtn.addEventListener('click', exportToExcel);
 });
 
-// 既存のxlsxファイルを読み込む関数
 function readExistingXLSXFile(filePath) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
         xhr.open('GET', filePath, true);
         xhr.responseType = 'arraybuffer';
-
         xhr.onload = function () {
             if (xhr.status === 200) {
                 const data = new Uint8Array(xhr.response);
-                const workbook = XLSX.read(data, {type: 'array'});
+                const workbook = XLSX.read(data, {type: 'array', cellStyles: true, cellDates: true});
                 resolve(workbook);
             } else {
                 reject(`Failed to load the file. Status: ${xhr.status}`);
             }
         };
-
         xhr.onerror = function() {
             reject('An error occurred while reading the file.');
         };
-
         xhr.send();
     });
 }
 
 function exportToExcel() {
-    // 既存のExcelファイルを読み込む
-    readExistingXLSXFile('/static/飛行日誌1.xlsx').then(workbook => {
-        // 既存のWorkbookをベースとして、新しいSheetを追加する
-        const ws = XLSX.utils.table_to_sheet(document.querySelector('.maintenance-table'));
-        XLSX.utils.book_append_sheet(workbook, ws, "MaintenanceRecord");
+    readExistingXLSXFile('/static/飛行日誌1.xlsx').then(originalWorkbook => {
+        const originalSheet = originalWorkbook.Sheets[originalWorkbook.SheetNames[0]];
 
-        // ○印を入れる
+        // ○印を入れる部分のみを更新
         for (let row = 1; row <= 9; row++) {
             const checkbox = document.querySelector(`.maintenance-table tbody tr:nth-child(${row}) td:nth-child(3) input`);
             if (checkbox && checkbox.checked) {
-                ws[XLSX.utils.encode_cell({c: 2, r: row})].v = "○";
+                const cellRef = XLSX.utils.encode_cell({c: 2, r: row});
+                originalSheet[cellRef] = {v: "○", t: 's', s: (originalSheet[cellRef] || {}).s };
             }
         }
 
-        // 新しいExcelファイルとして保存
-        XLSX.writeFile(workbook, "Modified飛行日誌1.xlsx");
+        // 既存のデータやスタイルを変更しないため、これ以降のrows.forEachの部分は削除
+
+        XLSX.writeFile(originalWorkbook, "Modified飛行日誌1.xlsx");
     }).catch(error => {
         console.error(error);
     });
 }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const exportBtn = document.getElementById('export-maintenance-excel');
+//     exportBtn.addEventListener('click', exportToExcel);
+// });
+
+// function readExistingXLSXFile(filePath) {
+//     return new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open('GET', filePath, true);
+//         xhr.responseType = 'arraybuffer';
+//         xhr.onload = function () {
+//             if (xhr.status === 200) {
+//                 const data = new Uint8Array(xhr.response);
+//                 const workbook = XLSX.read(data, {type: 'array', cellStyles: true, cellDates: true});
+//                 resolve(workbook);
+//             } else {
+//                 reject(`Failed to load the file. Status: ${xhr.status}`);
+//             }
+//         };
+//         xhr.onerror = function() {
+//             reject('An error occurred while reading the file.');
+//         };
+//         xhr.send();
+//     });
+// }
+
+// function exportToExcel() {
+//     readExistingXLSXFile('/static/飛行日誌1.xlsx').then(originalWorkbook => {
+//         const originalSheet = originalWorkbook.Sheets[originalWorkbook.SheetNames[0]];
+//         const table = document.querySelector('.maintenance-table');
+//         const rows = table.querySelectorAll('tbody tr');
+        
+//         // ○印を入れる
+//         for (let row = 1; row <= 9; row++) {
+//             const checkbox = document.querySelector(`.maintenance-table tbody tr:nth-child(${row}) td:nth-child(3) input`);
+//             if (checkbox && checkbox.checked) {
+//                 const cellRef = XLSX.utils.encode_cell({c: 2, r: row});
+//                 originalSheet[cellRef] = {v: "○", t: 's'};
+//             }
+//         }
+
+//         rows.forEach((row, rowIndex) => {
+//             const cells = row.querySelectorAll('td');
+//             cells.forEach((cell, cellIndex) => {
+//                 const cellValue = cell.textContent || cell.innerText;
+//                 if (cellValue) {
+//                     const cellRef = XLSX.utils.encode_cell({c: cellIndex, r: rowIndex + 1});
+//                     originalSheet[cellRef] = {
+//                         v: cellValue,
+//                         t: 's',
+//                         s: (originalSheet[XLSX.utils.encode_cell({c: cellIndex, r: rowIndex})] || {}).s
+//                     };
+//                 }
+//             });
+//         });
+
+//         XLSX.writeFile(originalWorkbook, "Modified飛行日誌1.xlsx");
+//     }).catch(error => {
+//         console.error(error);
+//     });
+// }
